d091660de4b729b4dd6cecc8c3f825fea3022c3f
batondesu <trantoan29072003@gmail.com>
2023-11-13 01:33:49 +0700

    dựng khung

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Trọ Tốt</title>
+    <link rel="shortcut icon" href="images/logo1.png" />
+</head>
+<body>
+    <section>
+        <?php include "header.php"?>
+        
+        <div class="banner">
+			<div class="owl-carousel owl-theme"></div>
+		</div> 
+
+        <?php include "top_room.php"?>
+    </section>
+
+    <script src="js/main.js"> </script>
+</body>
+</html>
