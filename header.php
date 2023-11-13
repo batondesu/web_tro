065aa4074b67004706553395640f72ac91d0497f
batondesu <trantoan29072003@gmail.com>
2023-11-13 15:54:33 +0700

    version 1

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Trọ Tốt</title>
+    <link rel="shortcut icon" href="images/logo1.png" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/header.css"> 
+</head>
+<body>
+
+    <section id="header">
+        <div class="header">
+            <div class="container">
+                <div class="container-inner full">
+                    <div class="logo">
+                        <a href="index.php?">
+                            <img src="images/logo.png" alt="Trang chủ" title="Trang chủ">
+                            <i> Trọ Tốt </i>
+                        </a>
+                    </div> 
+                    <!-- End Logo -->
+                    <div class="menu">
+                        <ul class="nav-top">
+                            <li><a href="search.php?type=Phòng%20trọ"> PHÒNG TRỌ</a></li>
+                            <li><a href="search.php?type=Nhà%20nguyên%20căn"> NHÀ NGUYÊN CĂN</a></li>
+                            <li><a href="search.php?type=Trọ%20ghép"> TRỌ GHÉP</a></li>
+                            <li>
+                                <a href="login.php?name_url=room_post"> ĐĂNG BÀI</a> 
+                            </li>
+                        </ul> 
+                    </div> 
+                <!-- End Menu -->
+                <div class="box-user">
+                    <ul class="nav-inout">
+                        <li class="signup">
+                            <a class="dangky" href="signup.php"> ĐĂNG KÝ</a>
+                        </li>
+                        <li class="login">
+                            <a class="dangnhap" href="login.php"> ĐĂNG NHẬP</a> 
+                        </li>
+                    </ul>                 
+                </div>
+            </div>
+        </div>
+    </div> 
+    <!-- End Header -->
+</body>
+</html>
