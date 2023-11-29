65c2bf025bce93508a15b442f80a3cb6acdff515
batondesu <trantoan29072003@gmail.com>
2023-11-29 14:48:04 +0700

    admin page

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['user_id']) && isset($_COOKIE['user_id'])) {
+        $user_id = $_COOKIE['user_id'];
+        $_SESSION['user_id'] = $user_id;
+        //echo "cvl";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="vi">
 <head>
@@ -6,40 +16,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Trọ Tốt</title>
-    <link rel="shortcut icon" type="image/png" href="images/favicon.png" />
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" crossorigin="anonymous">
+    <link rel="shortcut icon" href="images/logo.png" />
     
-    <!-- css -->
-    <link rel="stylesheet" href="css/trangchu.css"> 
-    <link rel="stylesheet" href="css/header.css"> 
+    <!-- url icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" crossorigin="anonymous"> 
 
+    <!-- css -->
+    <link rel="stylesheet" href="css/trangchu.css">
+    <link rel="stylesheet" href="css/header.css">
+    
     <!-- js -->
     <script src="js/main.js"> </script>
-    <script src="js/home.js"></script>
+    <script src="js/home.js"> </script>
 
     <!-- lib owl carousel -->
     <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
     <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="owlcarousel/owl.carousel.min.js"></script>
+
+
+
 </head>
 <body>
     <section>
+        <!-- 
+        <script> addHeader(); </script>-->
         <?php include "header.php"?>
         
         <div class="banner">
 			<div class="owl-carousel owl-theme"></div>
 		</div> 
+        
+         <script> addList(); </script> 
 
-        <script> addList(); </script> 
 
         <?php include "top_room.php"?>
 
-        <script> addReport(); </script> 
-        <script> addFooter(); </script> 
+        <script> addReport(); </script>
+        <script> addFooter(); </script>
+
 
     </section>
 
 </body>
+
+    <script></script>
+
 </html>
